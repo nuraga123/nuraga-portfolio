c65975a332686bc84c5a096a42d04117e16bce7a
nuraga123 <nuragayusifli@gmail.com>
2026-01-01 20:48:05 +0200

    fix

---
+++ src/pages/soft/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function GoogleRedirect() {
+  useEffect(() => {
+    window.location.href =
+      "https://mega.nz/folder/CuYGXA6Y#hidzukixYkFuBrSgkm9s4g";
+  }, []);
+
+  return (
+    <div>
+      <p>загрузка...</p>
+      <a href="https://mega.nz/folder/CuYGXA6Y#hidzukixYkFuBrSgkm9s4g"></a>
+    </div>
+  );
+}
